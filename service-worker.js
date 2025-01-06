f03c444f0d870232563cf7278de3b35d76707a26
Pedro Martins <pedro.olmartins@outlook.com>
2025-01-05 22:49:11 -0300

    atualiza caminhos dos arquivos no service worker para remover o prefixo da pasta 'financas'

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,16 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('meu-app-cache').then((cache) => {
             return cache.addAll([
-                'financas/',
-                'financas/index.html',
-                'financas/style/styles.css',
-                'financas/js/scripts.js',
-                'financas/icons/icon-120x120.png',
-                'financas/icons/icon-152x152.png',
-                'financas/icons/icon-167x167.png',
-                'financas/icons/icon-180x180.png',
-                'financas/icons/icon-192x192.png',
-                'financas/icons/icon-512x512.png'
+                '',
+                'index.html',
+                'style/styles.css',
+                'js/scripts.js',
+                'icons/icon-120x120.png',
+                'icons/icon-152x152.png',
+                'icons/icon-167x167.png',
+                'icons/icon-180x180.png',
+                'icons/icon-192x192.png',
+                'icons/icon-512x512.png'
             ]);
         })
     );
